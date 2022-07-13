456a337a7475795d8611e3b96c93c69d8bc1223a
Ricardo3113 <ricardofariasdesouza@hotmail.com>
2022-07-13 17:52:43 -0300

    Security config

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -11,7 +11,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    <a href="https://www.linkedin.com/in/ricardo-farias-de-souza-2501614a/" target={'_blank'}> Ricardo Farias de Souza</a>
                 </p>
             </div>
         </header>
